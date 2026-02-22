6688721530c71183e3e3bbaaee2dfe7b83775025
AhmedBaid <baidahmed2005@gmail.com>
2026-02-22 11:35:51 +0000

    Chifoumi

--- Chifoumi/Chifoumi.java
+++ Chifoumi/Chifoumi.java
@@ -1,3 +1,18 @@
-public enum ChifoumiAction {
-    ROCK, PIPER, SCISSOR
+public class Chifoumi {
+    public static ChifoumiAction getActionBeatenBy(ChifoumiAction chifoumiAction) {
+        switch (chifoumiAction) {
+            case ROCK:
+                return ChifoumiAction.SCISSOR;
+            case PAPER:
+                return ChifoumiAction.ROCK;
+            default:
+                return ChifoumiAction.PAPER;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Chifoumi.getActionBeatenBy(ChifoumiAction.ROCK));
+        System.out.println(Chifoumi.getActionBeatenBy(ChifoumiAction.PAPER));
+        System.out.println(Chifoumi.getActionBeatenBy(ChifoumiAction.SCISSOR));
+    }
 }
--- Chifoumi/ChifoumiAction.java
+++ Chifoumi/ChifoumiAction.java
@@ -1,12 +1,3 @@
-public class Chifoumi {
-    public static ChifoumiAction getActionBeatenBy(ChifoumiAction chifoumiAction) {
-        switch (chifoumiAction) {
-            case ROCK:
-                return ChifoumiAction.SCISSOR;
-            case PIPER:
-                return ChifoumiAction.ROCK;
-            default:
-                return ChifoumiAction.PIPER;
-        }
-    }
+public enum ChifoumiAction {
+    ROCK, PAPER, SCISSOR
 }
