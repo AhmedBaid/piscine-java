2c4a199af24aa9b8c2ce4c9cec046d44143d7611
AhmedBaid <baidahmed2005@gmail.com>
2026-02-22 15:09:09 +0000

    Palindrome

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,8 +1,14 @@
 public class Palindrome {
     public static boolean isPalindrome(String s) {
-        if (s==null){
-            return false;
-        }
-        return new StringBuilder(s) == new StringBuilder(s).reverse();
+        String s1 = new StringBuilder(s).toString();
+        String s2 = new StringBuilder(s).reverse().toString();
+
+
+        return s1.equals(s2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Palindrome.isPalindrome("ressasser"));
+        System.out.println(Palindrome.isPalindrome("Bonjour"));
     }
 }
