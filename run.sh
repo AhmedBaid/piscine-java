67e2284d7885bde28b19153018c862c2f6f6f747
AhmedBaid <baidahmed2005@gmail.com>
2026-02-22 11:06:37 +0000

    IntOperations

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+# Check if argument exists
+if [ -z "$1" ]; then
+    echo "Usage: $0 folder/file.java"
+    exit 1
+fi
+
+# Full path
+full_path="$1"
+
+# Extract folder (before last /)
+folder=$(dirname "$full_path")
+
+# Create folder
+mkdir -p "$folder"
+
+# Create file
+touch "$full_path"
+
