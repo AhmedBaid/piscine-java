f8a86932b7a62813176f10a38430700dd577e8f3
AhmedBaid <baidahmed2005@gmail.com>
2026-02-22 11:42:51 +0000

    StringLength

---
+++ StringLength/StringLength.java
@@ -0,0 +1,10 @@
+public class StringLength {
+    public static int getStringLength(String s) {
+        return s.length();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(StringLength.getStringLength("Hello World !"));
+        System.out.println(StringLength.getStringLength(""));
+    }
+}
