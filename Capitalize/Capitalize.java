4c50d80f325c9c72f9b478c8c30e540462518021
AhmedBaid <baidahmed2005@gmail.com>
2026-02-23 14:37:24 +0000

    capitilize

--- Capitalize/Capitalize.java
+++ Capitalize/Capitalize.java
@@ -15,10 +15,10 @@ public class Capitalize {
             List<String> newSl = new ArrayList<>();
             for (String ele : sl) {
                 if (ele.length() > 0) {
-                    newSl.add(ele.substring(0, 1).toUpperCase() + ele.substring(1, ele.length()));
+                    newSl.add(ele.substring(0, 1).toUpperCase() + ele.substring(1, ele.length()).toLowerCase());
                 }
             }
-            Files.writeString(distination, String.join(" ", newSl));
+            Files.writeString(distination, String.join(" ", newSl).trim());
         }
     }
 
