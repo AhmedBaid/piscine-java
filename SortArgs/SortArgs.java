910826026ef248d01cd6b94951d835f0c78885a4
AhmedBaid <baidahmed2005@gmail.com>
2026-02-23 13:00:09 +0000

    sortArgs

--- SortArgs/SortArgs.java
+++ SortArgs/SortArgs.java
@@ -3,6 +3,6 @@ import java.util.Arrays;
 public class SortArgs {
     public static void sort(String[] args) {
         Arrays.sort(args);
-        System.out.println(args);
+        System.out.println(String.join(" ", args));
     }
 }
