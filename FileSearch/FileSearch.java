89c4738ae927df9a79bc6b70a5c4eb4967d0f74b
AhmedBaid <baidahmed2005@gmail.com>
2026-02-24 11:31:07 +0000

    CelestialObject

--- FileSearch/FileSearch.java
+++ FileSearch/FileSearch.java
@@ -1,8 +1,5 @@
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class FileSearch {
     public static String searchFile(String fileName) {
