af1040972c04fb63da91568d143f609388de796a
AhmedBaid <baidahmed2005@gmail.com>
2026-03-01 11:13:09 +0000

    Character

---
+++ AdventureCharacter/Character.java
@@ -0,0 +1,58 @@
+public class Character {
+    public final int maxHealth;
+    public int currentHealth;
+    public String name;
+
+    public int GetmaxHealth() {
+        return maxHealth;
+    }
+
+    public int GetcurrentHealth() {
+        return currentHealth;
+    }
+
+    public String GetmaxName() {
+        return name;
+    }
+
+    public Character(String name, int maxHealth) {
+        this.name = name;
+        this.maxHealth = maxHealth;
+        this.currentHealth = maxHealth;
+    }
+
+    @Override
+    public String toString() {
+        if (this.currentHealth <= 0) {
+            return String.format("%s : KO", this.name);
+        } else {
+            return String.format("%s : %d/%d", this.name, this.currentHealth, this.maxHealth);
+        }
+    }
+
+    public void takeDamage(int health) {
+        if (this.currentHealth - health <= 0) {
+            this.currentHealth -= health;
+        }
+    }
+
+    public void attack(Character charchter) {
+        charchter.takeDamage(9);
+    }
+
+    public static void main(String[] args) {
+        Character aragorn = new Character("Aragorn", 20);
+        Character uruk = new Character("Uruk", 5);
+
+        System.out.println(aragorn.toString());
+        System.out.println(uruk.toString());
+
+        aragorn.attack(uruk);
+
+        System.out.println(uruk.toString());
+
+        aragorn.takeDamage(12);
+
+        System.out.println(aragorn.toString());
+    }
+}
