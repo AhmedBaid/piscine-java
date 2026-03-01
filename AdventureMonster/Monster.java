107207d2d3c6aeebf5315a17b58058073f25c70c
AhmedBaid <baidahmed2005@gmail.com>
2026-03-01 12:38:20 +0000

    monster

--- AdventureMonster/Monster.java
+++ AdventureMonster/Monster.java
@@ -7,9 +7,9 @@ public class Monster extends Character {
     @Override
     public String toString() {
         if (this.getCurrentHealth() == 0) {
-            return String.format("%s is a monster with %s HP", this.getName(), this.getCurrentHealth());
-        } else {
             return String.format("%s is a monster and is dead", this.getName());
+        } else {
+            return String.format("%s is a monster with %s HP", this.getName(), this.getCurrentHealth());
         }
     }
 
