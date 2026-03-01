1239d1c2cf30311f54708c9d7536d6568774584e
AhmedBaid <baidahmed2005@gmail.com>
2026-03-01 12:36:56 +0000

    mosnter

---
+++ AdventureMonster/Character.java
@@ -0,0 +1,93 @@
+import java.util.*;
+
+public class Character {
+    private final int maxHealth;
+    private int currentHealth;
+    private final String name;
+    private static List<Character> allCharacters = new ArrayList<>();
+
+    public Character(String name, int maxHealth) {
+        this.name = name;
+        this.maxHealth = maxHealth;
+        this.currentHealth = maxHealth;
+        allCharacters.add(this);
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        if (this.currentHealth <= 0) {
+            return String.format("%s : KO", this.name);
+        } else {
+            return String.format("%s : %d/%d", this.name, this.currentHealth, this.maxHealth);
+        }
+    }
+
+    public void takeDamage(int health) {
+        if (this.currentHealth - health >= 0) {
+            this.currentHealth -= health;
+        } else {
+            this.currentHealth = 0;
+        }
+    }
+
+    public void attack(Character charchter) {
+        charchter.takeDamage(9);
+    }
+
+    public static String printStatus() {
+        StringBuilder res = new StringBuilder();
+        if (allCharacters.size() > 0) {
+            res.append("------------------------------------------\n" +
+                    "Characters currently fighting :" + "\n");
+            for (Character charr : allCharacters) {
+                res.append(" - " + charr.toString() + "\n");
+            }
+            res.append("------------------------------------------" + "\n");
+            return res.toString();
+        } else {
+            res.append("------------------------------------------\n" +
+                    "Nobody's fighting right now !\n" +
+                    "------------------------------------------" + "\n");
+            return res.toString();
+        }
+    }
+
+    public static Character fight(Character char1, Character char2) {
+        while (true) {
+            char1.attack(char2);
+            if (char2.getCurrentHealth() <= 0) {
+                return char1;
+            }
+            char2.attack(char1);
+            if (char1.getCurrentHealth() <= 0) {
+                return char2;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.print(Character.printStatus());
+
+        Character aragorn = new Character("Aragorn", 20);
+        Character uruk = new Character("Uruk", 15);
+
+        System.out.print(Character.printStatus());
+
+        Character winner = Character.fight(aragorn, uruk);
+
+        System.out.println(winner.toString());
+        System.out.print(Character.printStatus());
+    }
+}
